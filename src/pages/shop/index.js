9131e13fb586eb544381c417de895bad08348148
Okkar Min <omin001@e.ntu.edu.sg>
2021-06-09 22:30:33 +0800

    fix: :bug: Fix serverside build errors

--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -1,56 +1,74 @@
 import Link from "next/link";
-import { server } from "config.js";
+import useSWR from "swr";
 
-import { Container, Center, Flex, SimpleGrid } from "@chakra-ui/react";
+import { Container, Center, Flex, SimpleGrid, Spinner } from "@chakra-ui/react";
 
 import { Header } from "../../components/layout/Header";
 import ShopItemCard from "../../components/cards/ShopItemCard";
 
-const Shop = ({ shopItems }) => {
-  return (
-    <Center backgroundColor="background">
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const Shop = () => {
+  const { data: shopItems, error } = useSWR("/api/shop", fetcher);
+
+  if (error) return <div>Error</div>;
+  if (!shopItems)
+    return (
       <Flex
         direction="column"
-        width={["100vw", "60vw"]}
-        backgroundColor="background"
+        height="100vh"
+        justifyContent="center"
+        alignItems="center"
       >
-        <Header />
-        <Container marginTop="6">
-          <SimpleGrid
-            alignItems="center"
-            justifyContent="center"
-            //spacing="6"
-            columns={[1, 2, 2]}
-            spacingX="350"
-            spacingY="6"
-            marginRight={["0", "80"]}
-          >
-            {shopItems.map((item, i) => {
-              return (
-                <Link href={`/shop/${item.slug}`} key={i} passHref>
-                  <ShopItemCard itemName={item.name} value={item.value} />
-                </Link>
-              );
-            })}
-          </SimpleGrid>
-        </Container>
+        <Spinner />
       </Flex>
-    </Center>
-  );
+    );
+
+  if (shopItems)
+    return (
+      <Center backgroundColor="background">
+        <Flex
+          direction="column"
+          width={["100vw", "60vw"]}
+          backgroundColor="background"
+        >
+          <Header />
+          <Container marginTop="6">
+            <SimpleGrid
+              alignItems="center"
+              justifyContent="center"
+              //spacing="6"
+              columns={[1, 2, 2]}
+              spacingX="350"
+              spacingY="6"
+              marginRight={["0", "80"]}
+            >
+              {shopItems.map((item, i) => {
+                return (
+                  <Link href={`/shop/${item.slug}`} key={i} passHref>
+                    <ShopItemCard itemName={item.name} value={item.value} />
+                  </Link>
+                );
+              })}
+            </SimpleGrid>
+          </Container>
+        </Flex>
+      </Center>
+    );
 };
 
 export default Shop;
 
-export async function getStaticProps() {
-  const response = await fetch(`${server}/api/shop`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  const shopItems = await response.json();
+// export async function getStaticProps() {
+//   const response = await fetch(`${server}/api/shop`, {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
+//   const shopItems = await response.json();
 
-  return {
-    props: { shopItems },
-  };
-}
+//   return {
+//     props: { shopItems },
+//   };
+// }
