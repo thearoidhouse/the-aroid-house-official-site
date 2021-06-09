7a7580439b1ea8872a496a7b8a62bc3f0c4401d2
Okkar Min <omin001@e.ntu.edu.sg>
2021-06-09 17:26:48 +0800

    fix: :bug: Fix a bug where Link complains that ref is not being passed in

--- src/components/buttons/BigButton.js
+++ src/components/buttons/BigButton.js
@@ -1,26 +1,29 @@
+import React from "react";
 import Link from "next/link";
 import { Button } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
 const MotionButton = motion(Button);
 
-export const BigButton = (props) => {
+export const BigButton = React.forwardRef(function BigButton(props, ref) {
   return (
-    <MotionButton
-      backgroundColor="buttonBackground"
-      textColor="background"
-      fontSize="lg"
-      width={["50vw", "25vw"]}
-      _hover={{
-        backgroundColor: "buttonBackgroundHover",
-      }}
-      padding="7"
-      borderRadius="5"
-      whileHover={{ scale: 1.1 }}
-      fontWeight="bold"
-      {...props}
-    >
-      {props.name}
-    </MotionButton>
+    <span href={props.href} ref={ref}>
+      <MotionButton
+        backgroundColor="buttonBackground"
+        textColor="background"
+        fontSize="lg"
+        width={["50vw", "25vw"]}
+        _hover={{
+          backgroundColor: "buttonBackgroundHover",
+        }}
+        padding="7"
+        borderRadius="5"
+        whileHover={{ scale: 1.1 }}
+        fontWeight="bold"
+        {...props}
+      >
+        {props.name}
+      </MotionButton>
+    </span>
   );
-};
+});
--- src/components/buttons/SmallButton.js
+++ src/components/buttons/SmallButton.js
@@ -1,26 +1,29 @@
+import React from "react";
 import Link from "next/link";
 import { Button } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
 const MotionButton = motion(Button);
 
-export const SmallButton = (props) => {
+export const SmallButton = React.forwardRef(function SmallButton(props, ref) {
   return (
-    <MotionButton
-      backgroundColor="buttonBackground"
-      textColor="background"
-      fontSize="lg"
-      width={["30vw", "25vw"]}
-      _hover={{
-        backgroundColor: "buttonBackgroundHover",
-      }}
-      padding="7"
-      borderRadius="5"
-      whileHover={{ scale: 1.1 }}
-      fontWeight="bold"
-      {...props}
-    >
-      {props.name}
-    </MotionButton>
+    <span href={props.href} ref={ref}>
+      <MotionButton
+        backgroundColor="buttonBackground"
+        textColor="background"
+        fontSize="lg"
+        width={["30vw", "25vw"]}
+        _hover={{
+          backgroundColor: "buttonBackgroundHover",
+        }}
+        padding="7"
+        borderRadius="5"
+        whileHover={{ scale: 1.1 }}
+        fontWeight="bold"
+        {...props}
+      >
+        {props.name}
+      </MotionButton>
+    </span>
   );
-};
+});
