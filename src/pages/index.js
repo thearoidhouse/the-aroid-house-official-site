e087b30d5a49441e2e3974c48842e2833440fa14
dannyyys <dannyyys@users.noreply.github.com>
2021-06-11 02:11:50 +0800

    style: :lipstick: Update text and add link to insta

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import { Text, Flex, Heading } from "@chakra-ui/react";
 import { React } from "react";
 import Link from "next/link";
+import { Link as Link2 } from "@chakra-ui/react";
 
 import { BigButton } from "../components/buttons/BigButton";
 import Landing from "../components/Landing";
@@ -15,11 +16,21 @@ const Index = () => (
     <Heading marginTop="20" color="text">
       TheAroidHouse
     </Heading>
+    <Flex direction="row" marginTop="5">
+      <Heading color="text" fontSize="xl">
+        by{""}
+      </Heading>
+      <Link2 href="https://www.instagram.com/marcusonaroids/" isExternal>
+        <Heading color="text" fontSize="xl">
+          @marcusonaroids
+        </Heading>
+      </Link2>
+    </Flex>
 
     <Landing marginTop="10" />
 
     <Text marginTop="4" fontSize="xl" color="text">
-      We try to make gardening simple
+      Trying to make gardening simple.
     </Text>
 
     <Link href="/shop">
