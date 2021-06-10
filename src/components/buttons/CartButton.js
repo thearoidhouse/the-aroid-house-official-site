d540be3f09984de5aba584dd894988194416da73
dannyyys <dannyyys@users.noreply.github.com>
2021-06-10 17:54:16 +0800

    refactor: :sparkles: Add cart show no bubble when it is empty

--- src/components/buttons/CartButton.js
+++ src/components/buttons/CartButton.js
@@ -5,6 +5,11 @@ import { CartContext } from "../../context/CartContext";
 
 const CartButton = React.forwardRef(function Cart({ onClick, href }, ref) {
   const [items, setItems] = useContext(CartContext);
+
+  const cartItemsCount = items.reduce((accumulator, cartItem) => {
+    return accumulator + cartItem.quantity;
+  }, 0);
+
   return (
     <span href={href} onClick={onClick} ref={ref}>
       <chakra.span
@@ -24,24 +29,25 @@ const CartButton = React.forwardRef(function Cart({ onClick, href }, ref) {
         >
           <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
         </Icon>
-        <chakra.span
-          pos="absolute"
-          top="-1px"
-          right="-1px"
-          px={2}
-          py={1}
-          fontSize="xs"
-          fontWeight="bold"
-          lineHeight="none"
-          color="background"
-          transform="translate(50%,-50%)"
-          bg="text"
-          rounded="full"
-        >
-          {items.reduce((accumulator, cartItem) => {
-            return accumulator + cartItem.quantity;
-          }, 0)}
-        </chakra.span>
+
+        {cartItemsCount > 0 && (
+          <chakra.span
+            pos="absolute"
+            top="-1px"
+            right="-1px"
+            px={2}
+            py={1}
+            fontSize="xs"
+            fontWeight="bold"
+            lineHeight="none"
+            color="background"
+            transform="translate(50%,-50%)"
+            bg="text"
+            rounded="full"
+          >
+            {cartItemsCount}
+          </chakra.span>
+        )}
       </chakra.span>
     </span>
   );
