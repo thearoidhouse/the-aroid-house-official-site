a3691c646097d263f7f051d577f0f60c06169c0d
Okkar Min <omin001@e.ntu.edu.sg>
2021-06-10 10:39:57 +0800

    feat: :sparkles: Implement static page generation for each shop item

--- src/pages/shop/[itemSlug].js
+++ src/pages/shop/[itemSlug].js
@@ -1,63 +1,55 @@
-import { useRouter } from "next/router";
-import useSWR from "swr";
-
-import { Center, Flex, Box, Spinner } from "@chakra-ui/react";
+import { server } from "config.js";
+import { Center, Flex } from "@chakra-ui/react";
 
 import { Header } from "../../components/layout/Header";
 import { ItemCarousel } from "../../components/item/ItemCarousel";
 import { ItemDetails } from "../../components/item/ItemDetails";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
-function ItemSlug() {
-  const { itemSlug } = useRouter().query;
-  const { data, error } = useSWR(`/api/shop/${itemSlug}`, fetcher);
-
-  if (error) return <div>failed to load</div>;
-  if (!data)
-    return (
+function ItemSlug({ itemDetails }) {
+  return (
+    <Center backgroundColor="background">
       <Flex
-        direction="column"
         height="100vh"
-        justifyContent="center"
-        alignItems="center"
+        direction="column"
+        backgroundColor="background"
+        width={["100vw", "60vw"]}
       >
-        <Spinner />
-      </Flex>
-    );
-
-  if (data)
-    return (
-      <Center backgroundColor="background">
+        <Header />
+        <Center>
+          <ItemCarousel />
+        </Center>
         <Flex
-          height="100vh"
+          backgroundColor="text"
+          height="full"
+          borderRadius="20px 20px 0px 0px"
           direction="column"
-          //alignContent="center"
-          backgroundColor="background"
-          width={["100vw", "60vw"]}
         >
-          <Header />
-          <Center>
-            <ItemCarousel />
-          </Center>
-          <Flex
-            backgroundColor="text"
-            height="full"
-            borderRadius="20px 20px 0px 0px"
-            direction="column"
-            //padding="5"
-            //marginTop="10"
-          >
-            <ItemDetails
-              shopItemName={data.name}
-              shopItemDescription={data.description}
-              shopItemVariants={data.variants}
-              shopItemSlug={data.slug}
-            />
-          </Flex>
+          <ItemDetails
+            shopItemName={itemDetails.name}
+            shopItemDescription={itemDetails.description}
+            shopItemVariants={itemDetails.variants}
+            shopItemSlug={itemDetails.slug}
+          />
         </Flex>
-      </Center>
-    );
+      </Flex>
+    </Center>
+  );
 }
 
 export default ItemSlug;
+
+export async function getStaticPaths() {
+  const response = await fetch(`${server}/api/shop/getShopItemSlugs`);
+  const slugs = await response.json();
+
+  const paths = slugs.map((slug) => ({ params: { itemSlug: slug } }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  const response = await fetch(`${server}/api/shop/${params.itemSlug}`);
+  const itemDetails = await response.json();
+
+  return { props: { itemDetails } };
+}
