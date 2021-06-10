6152f128e60e80bd91d2697feb00efa164cf952d
Okkar Min <omin001@e.ntu.edu.sg>
2021-06-10 14:35:42 +0800

    feat: :sparkles: Implement telegram bot message being sent on new order

--- domain/application/order/new-order.tsx
+++ domain/application/order/new-order.tsx
@@ -1,4 +1,6 @@
+import { server } from "config";
 import { OrderAggregate } from "domain/models/aggregates/OrderAggregate";
+import { CartItem } from "domain/models/entities/CartItem";
 import { IOrderRepo } from "domain/models/infrastructure/IOrderRepository";
 
 interface INewOrder {
@@ -8,6 +10,31 @@ interface INewOrder {
 
 export const newOrder = async ({ orderRepo, orderAggregate }: INewOrder) => {
   // send telegram message
+  const craftTextFromOrder = (orderAggregate: OrderAggregate) => {
+    let message = `🛍 *New Order*\n⏱ ${new Date().toLocaleString()}\n\n`;
+
+    orderAggregate.cart.cartItems.map((cartItem: CartItem) => {
+      message = message.concat(
+        `👉 ${cartItem.shopItemName} ${cartItem.variant} x ${cartItem.quantity}\n`
+      );
+    });
+    return message;
+  };
+
+  const botMessage = {
+    message: {
+      chat: {
+        id: 12345,
+      },
+      text: craftTextFromOrder(orderAggregate),
+    },
+  };
+
+  fetch(`${server}/api/webhook`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(botMessage),
+  });
 
   // send order confirmation email
 
