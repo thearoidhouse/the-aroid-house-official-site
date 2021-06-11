ab4133f4ce823dbdb6716cbc13387f0842feb55b
dannyyys <dannyyys@users.noreply.github.com>
2021-06-11 20:47:24 +0800

    feat: :sparkles: Add metatags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,28 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { ColorModeScript } from '@chakra-ui/react'
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <title>TheAroidHouse</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="title" content="TheAroidHouse" />
+          <meta name="description" content="Trying to make gardening simple." />
+
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:url"
+            content="https://the-aroid-house-official-site.vercel.app/"
+          />
+          <meta property="og:title" content="TheAroidHouse" />
+          <meta
+            property="og:description"
+            content="Trying to make gardening simple."
+          />
+          <meta property="og:image" content="/seo_image.png" />
+        </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
@@ -13,6 +30,6 @@ export default class Document extends NextDocument {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
