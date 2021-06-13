89fa80b31d6c3aeb5b0aa1d0e940dbd7ca33c224
dannyyys <dannyyys@users.noreply.github.com>
2021-06-13 16:46:28 +0800

    feat: :sparkles: Add carousel in landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,28 @@
-import { Text, Flex, Heading } from "@chakra-ui/react";
+import { Text, Flex, Heading, Center } from "@chakra-ui/react";
 import { React } from "react";
 import Link from "next/link";
 import { Link as Link2 } from "@chakra-ui/react";
 
 import { BigButton } from "../components/buttons/BigButton";
 import Landing from "../components/Landing";
+import { ItemCarousel } from "../components/item/ItemCarousel";
+
+const images = [
+  "https://images.unsplash.com/photo-1610906403539-eba004635227?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+  "https://images.unsplash.com/photo-1458410489211-ba19aa2f2902?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80",
+  "https://images.unsplash.com/photo-1579256353657-12115ca794b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80",
+  "https://images.unsplash.com/photo-1591534446608-95420ad57d8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80",
+  "https://images.unsplash.com/photo-1590452676666-2341e3587c18?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80",
+  "https://images.unsplash.com/photo-1598856671883-21156cd776a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80",
+  "https://images.unsplash.com/photo-1583340161290-5ab7db412816?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+  "https://images.unsplash.com/photo-1583378733378-3fee25067954?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80",
+  "https://images.unsplash.com/photo-1598856668774-f3a5f4dfdc89?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80",
+];
 
 const Index = () => (
   <Flex
-    height="110vh"
+    //height="110vh"
+    height="auto"
     direction="column"
     alignItems="center"
     backgroundColor="background"
@@ -44,6 +58,9 @@ const Index = () => (
     >
       <BigButton marginTop="5" name="Reviews" />
     </Link2>
+    <Center maxWidth={["100vw", "70vw", "50vw"]} marginTop="10">
+      <ItemCarousel images={images} />
+    </Center>
   </Flex>
 );
 
