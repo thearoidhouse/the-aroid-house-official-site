6e5cd6bcfc7036c24b090889d925fd5ba3948655
dannyyys <dannyyys@users.noreply.github.com>
2021-06-14 20:21:29 +0800

    feat: :sparkles: Add testinomial carousel in landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,15 +12,13 @@ import Landing from "../components/Landing";
 import { ItemCarousel } from "../components/item/ItemCarousel";
 
 const images = [
-  "https://images.unsplash.com/photo-1610906403539-eba004635227?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
-  "https://images.unsplash.com/photo-1458410489211-ba19aa2f2902?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80",
-  "https://images.unsplash.com/photo-1579256353657-12115ca794b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80",
-  "https://images.unsplash.com/photo-1591534446608-95420ad57d8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80",
-  "https://images.unsplash.com/photo-1590452676666-2341e3587c18?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80",
-  "https://images.unsplash.com/photo-1598856671883-21156cd776a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80",
-  "https://images.unsplash.com/photo-1583340161290-5ab7db412816?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
-  "https://images.unsplash.com/photo-1583378733378-3fee25067954?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80",
-  "https://images.unsplash.com/photo-1598856668774-f3a5f4dfdc89?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80",
+  "/testimonials/1.png",
+  "/testimonials/2.png",
+  "/testimonials/3.png",
+  "/testimonials/4.png",
+  "/testimonials/5.png",
+  "/testimonials/6.png",
+  "/testimonials/7.png",
 ];
 
 const Index = () => (
