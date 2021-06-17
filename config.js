e96c12d26cf196344f5cf7bce320d5d3da1db4e4
dannyyys <dannyyys@users.noreply.github.com>
2021-06-18 00:14:34 +0800

    fix: :bug: Update server

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev
-  ? "http://localhost:3000"
-  : "https://the-aroid-house.vercel.app";
+export const server = dev ? "http://localhost:3000" : process.env.VERCEL_URL;
